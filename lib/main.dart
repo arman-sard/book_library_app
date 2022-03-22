c359d54040cc36173070ebc3460ca5c27d7b17cb
arman <arman@sard.am>
2022-03-22 10:48:44 +0100

    Correcting  status Bar Color

--- lib/main.dart
+++ lib/main.dart
@@ -11,22 +11,24 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
-      statusBarIconBrightness: Brightness.dark,
-    ));
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.dark.copyWith(
+   statusBarColor: Colors.white, 
+   statusBarBrightness: Brightness.light 
+));
 
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.deepPurple,
         iconTheme: const IconThemeData(color: Colors.deepPurple),
-        dividerTheme: const DividerThemeData(space: 10, thickness: 1, color: Color.fromARGB(90, 124, 161, 154), indent: 20, endIndent: 20),
+        dividerTheme: const DividerThemeData(space: 20, thickness: 1, color: Color.fromARGB(90, 124, 161, 154), indent: 20, endIndent: 20),
+        buttonTheme: const ButtonThemeData(buttonColor: Colors.green),
         textTheme: const TextTheme(
           bodyText1: TextStyle(color: Colors.black, fontWeight: FontWeight.w400, fontSize: 14),
           bodyText2: TextStyle(color: Colors.grey, fontWeight: FontWeight.w400, fontSize: 13),
           subtitle1: TextStyle(color: Colors.black, fontWeight: FontWeight.w400, fontSize: 13),
-          headline6: TextStyle(fontSize: 15, color: Colors.deepPurple, fontWeight: FontWeight.bold),
-          button: TextStyle(fontSize: 12, fontWeight: FontWeight.w400) ,
+          headline6: TextStyle(color: Colors.deepPurple, fontWeight: FontWeight.bold, fontSize: 15),
+          button: TextStyle(color: Colors.deepPurple, fontWeight: FontWeight.w400, fontSize: 12,) ,
         ),
       ),
       home: const HomePage(),
